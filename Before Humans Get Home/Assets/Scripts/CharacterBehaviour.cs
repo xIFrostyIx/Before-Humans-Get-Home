2a2bcecfbef3feae47497bfcb96e063d2bd4c819
Joshua Guerrero <62621784+xIFrostyIx@users.noreply.github.com>
2024-08-29 13:32:06 -0500

    Added Jumping to Character Behavior

--- Before Humans Get Home/Assets/Scripts/CharacterBehaviour.cs
+++ Before Humans Get Home/Assets/Scripts/CharacterBehaviour.cs
@@ -6,11 +6,14 @@ public class CharacterBehaviour : MonoBehaviour
 {
     //Allows movement speed to be tweaked in unity
     public float movementSpeed;
+    public float jumpingForce;
 
     //references rigid body component
     private Rigidbody2D rb;
     //Sets default direction the player is facing
     private bool facingRight = true;
+    private float movementDirections;
+    private bool isJumping =  false;
 
     // Awake is called after all objects are initialized
     private void Awake()
@@ -22,6 +25,35 @@ public class CharacterBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //get inputs
+        movementDirections = Input.GetAxis("Horizontal"); // Scale of -1 to 1
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        //Animate
+        if (movementDirections > 0 && !facingRight)
+        {
+            flipCharacter();
+        }
+        else if (movementDirections < 0 && facingRight)
+        {
+            flipCharacter();
+        }
+
+        //movement
+        rb.velocity = new Vector2(movementDirections * movementSpeed, rb.velocity.y);
+        if(isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpingForce));
+        }
+        isJumping = false;
+    }
+
+    private void flipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 0f, 180f);
     }
 }
