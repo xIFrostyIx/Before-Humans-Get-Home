831d15a62f9e942aa8c4d7e1ad314099dfdda97a
Joshua Guerrero <62621784+xIFrostyIx@users.noreply.github.com>
2024-09-04 00:14:26 -0500

    Finished Timer
    When timer hits 00:00, Scene switches to lose scene

--- Before Humans Get Home/Assets/Scripts/Timer.cs
+++ Before Humans Get Home/Assets/Scripts/Timer.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 /*Created by: Joshua Guerrero
 /*Purpose: Script sets a timer that can be altered in the Inspector
 */
 
 public class Timer : MonoBehaviour
-{
+{	
    [SerializeField] TextMeshProUGUI timerText;
-   float elapsedTime;
+   [SerializeField] float remainingTime;
+	[SerializeField] string sceneName = "You Lose";
 
    void Update()
    {
-		  elapsedTime += Time.deltaTime;
-		  int minutes = Mathf.FloorToInt(elapsedTime / 60);
-		  int seconds = Mathf.FloorToInt(elapsedTime % 60);
-		  timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+		//Stops the timer from going into the negatives
+		if (remainingTime > 0)
+		{
+			remainingTime -= Time.deltaTime;
+		}
+		else if (remainingTime < 0)
+		{
+			remainingTime = 0;
+			//End Game function
+			SceneManager.LoadScene(sceneName);
+		}
+
+		//Formats the timer
+		 int minutes = Mathf.FloorToInt(remainingTime / 60);
+		 int seconds = Mathf.FloorToInt(remainingTime % 60);
+		 timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
    }
 }
